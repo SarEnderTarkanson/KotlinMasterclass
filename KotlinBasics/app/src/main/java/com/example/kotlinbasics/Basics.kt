50d29ed475e89e83896bbff17c56daae0743dac6
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-06 01:59:44 +0100

    a brief practice on float, Int, Long and Double

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,8 +1,8 @@
 package com.example.kotlinbasics
 
 fun main() {
-    var myAge = 38
-    println(myAge)
-    myAge = 39
-    println(myAge)
+    var pi = 3.14f
+    println(pi)
+    pi = 3.1556718769f
+    println(pi)
 }
---
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/FloatingTypes.kt
@@ -0,0 +1,12 @@
+package com.example.kotlinbasics
+
+fun main () {
+
+    var circleRatio = 3.1415926535
+
+    println(circleRatio)
+
+    circleRatio = 3.1415926535897932
+
+    println(circleRatio)
+}
