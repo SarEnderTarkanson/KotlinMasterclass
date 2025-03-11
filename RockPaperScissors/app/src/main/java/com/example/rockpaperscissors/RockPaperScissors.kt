0aadb1060d5207698b7e5bd97d17340b782cd17c
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-11 03:34:44 +0100

    built the logic to find out the winner and to present it

--- RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
+++ RockPaperScissors/app/src/main/java/com/example/rockpaperscissors/RockPaperScissors.kt
@@ -8,16 +8,29 @@ fun main() {
 
     playerChoice = readln()
 
-    val randomNumber = (1..3).random()
-
-    if (randomNumber == 1){
-        computerChoice = "Rock"
-    }
-    else if (randomNumber == 2){
-        computerChoice = "Paper"
-    }
-    else if(randomNumber==3){
-        computerChoice = "Scissors"
+    when ((1..3).random()) {
+        1 -> {
+            computerChoice = "Rock"
+        }
+        2 -> {
+            computerChoice = "Paper"
+        }
+        3 -> {
+            computerChoice = "Scissors"
+        }
     }
     println(computerChoice)
+
+    val winner = when {
+        playerChoice == computerChoice -> "Tie"
+        playerChoice == "Rock" && computerChoice == "Scissors" -> "Player"
+        playerChoice == "Paper" && computerChoice == "Rock" -> "Player"
+        playerChoice == "Scissors" && computerChoice == "Paper" -> "Player"
+        else -> "Computer"
+    }
+    if (winner == "Tie"){
+        println("It's a tie")
+    }else{
+        println("$winner won!")
+    }
 }
