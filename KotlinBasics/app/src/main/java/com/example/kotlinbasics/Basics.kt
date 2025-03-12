a226c2caac983d81aa24aed642031766638dcd7e
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-12 02:28:10 +0100

    a brief practice on while loop

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -2,17 +2,10 @@ package com.example.kotlinbasics
 
 fun main() {
 
-    var age = 0
-
-    println("Please enter your age as a whole number: ")
-    val enteredValue = readln()
-    age = enteredValue.toInt()
-
-    if (age in 18..39) {
-        println("You can go into the club")
-    } else if (age >= 40) {
-        println("You are too old")
-    } else {
-        println("You are too young to go into the club")
+    var count = 0
+    while (count <5){
+        println("Count is: $count")
+        count++
     }
+    println("Loop is done!")
 }
