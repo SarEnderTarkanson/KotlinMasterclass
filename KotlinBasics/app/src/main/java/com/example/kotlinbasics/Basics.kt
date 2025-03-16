0a4181824c46d8086e5b4474372e23926fbbae94
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-16 01:22:42 +0100

    added the name parameter to the function

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,16 +1,18 @@
 package com.example.kotlinbasics
 
 fun main() {
-makeCoffee(0)
+    makeCoffee(1, "Alpy")
+    makeCoffee(0, "Alpy")
+    makeCoffee(-1, "Alpy")
+    makeCoffee(2, "Alpy")
 }
 
-fun makeCoffee(sugarCount:Int){
-    if (sugarCount ==1 || sugarCount == 0){
-        println("Coffee with $sugarCount spoon of sugar")
-    } else if (sugarCount < 0){
-        println("You cannot have negative amount of spoons of sugar")
-    }
-    else {
-        println("Coffee with $sugarCount spoons of sugar")
+fun makeCoffee(sugarCount: Int, name: String) {
+
+    when {
+        sugarCount < 0 -> println("You cannot have a negative amount of spoons of sugar.")
+        sugarCount == 0 -> println("Coffee with no sugar for $name.")
+        sugarCount == 1 -> println("Coffee with $sugarCount spoon of sugar for $name.")
+        else -> println("Coffee with $sugarCount spoons of sugar for $name.")
     }
 }
