9de61820ba860a14f96c339e1434f88a15c25ff1
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-16 01:01:47 +0100

    just added an additional check for 0 in the control flow

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,11 +1,11 @@
 package com.example.kotlinbasics
 
 fun main() {
-makeCoffee(-1)
+makeCoffee(0)
 }
 
 fun makeCoffee(sugarCount:Int){
-    if (sugarCount ==1){
+    if (sugarCount ==1 || sugarCount == 0){
         println("Coffee with $sugarCount spoon of sugar")
     } else if (sugarCount < 0){
         println("You cannot have negative amount of spoons of sugar")
