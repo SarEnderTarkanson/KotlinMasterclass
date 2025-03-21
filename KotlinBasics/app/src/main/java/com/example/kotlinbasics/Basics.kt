eb5d55d3575533bf5a4c86aaf1b583593d6b8ddf
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-03-22 00:45:47 +0100

    Practice on data class

--- KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
+++ KotlinBasics/app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -1,12 +1,16 @@
 package com.example.kotlinbasics
 
+data class CoffeeDetails(
+    val sugarCount: Int,
+    val name: String,
+    val size: String,
+    val creamAmount: Int
+)
+
+
 fun main() {
-    var daisy = Dog("Daisy", "Dwarf Poodle", 15)
-    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age}")
-    daisy.age = 2
-    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age}")
-    daisy.name = "Meggi"
-    println("${daisy.name} is a ${daisy.breed} and is ${daisy.age}")
+    val coffeeForDennis = CoffeeDetails(0, "Dennis", "XXL", 0)
+    makeCoffee(coffeeForDennis)
 }
 
 
@@ -19,15 +23,15 @@ fun askCoffeeDetails() {
 
     var sugarCount = readln().toInt()
 
-    makeCoffee(sugarCount, name)
+    //makeCoffee(sugarCount, name)
 }
 
-fun makeCoffee(sugarCount: Int, name: String) {
+fun makeCoffee(coffeeDetails: CoffeeDetails) {
 
     when {
-        sugarCount < 0 -> println("You cannot have a negative amount of spoons of sugar.")
-        sugarCount == 0 -> println("Coffee with no sugar for $name.")
-        sugarCount == 1 -> println("Coffee with $sugarCount spoon of sugar for $name.")
-        else -> println("Coffee with $sugarCount spoons of sugar for $name.")
+        coffeeDetails.sugarCount < 0 -> println("You cannot have a negative amount of spoons of sugar.")
+        coffeeDetails.sugarCount == 0 -> println("Coffee with no sugar for ${coffeeDetails.name}.")
+        coffeeDetails.sugarCount == 1 -> println("Coffee with ${coffeeDetails.sugarCount} spoon of sugar for ${coffeeDetails.name}.")
+        else -> println("Coffee with ${coffeeDetails.sugarCount} spoons of sugar for ${coffeeDetails.name}.")
     }
 }
