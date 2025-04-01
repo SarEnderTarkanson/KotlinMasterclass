d3a76201a77de20c5538a68ba1fa6ed4211fc31f
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-01 02:20:15 +0200

    added an OutlinedTextField with an empty anonymous function.

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -8,8 +8,10 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -39,27 +41,18 @@ fun UnitConverter(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize()
     ) {
-
+        Text("Unit Converter")
+        OutlinedTextField(value= "Enter Value", onValueChange = {})
         Row {
-            Greeting(name = "Android")
-            Greeting(name = "Android")// Your content
+
         }
     }
-}
-
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     UnitConverterTheme {
-        Greeting("Android")
+
     }
 }
