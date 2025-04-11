c2e2e683d3363409445804a77ebecc6c37392e5d
Alparslan Tarkan <80176136+SarEnderTarkanson@users.noreply.github.com>
2025-04-12 01:27:27 +0200

    Added the Spacer for creating distance between the UI elements

--- UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
+++ UnitConverter/app/src/main/java/com/example/unitconverter/MainActivity.kt
@@ -8,6 +8,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
@@ -20,8 +21,8 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.node.InteroperableComposeUiNode
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.unitconverter.ui.theme.UnitConverterTheme
 
 class MainActivity : ComponentActivity() {
@@ -51,9 +52,11 @@ fun UnitConverter(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text("Unit Converter")
+        Spacer(modifier = Modifier.padding(16.dp))
         OutlinedTextField(value = "", onValueChange = {
-            
+
         })
+        Spacer(modifier = Modifier.padding(16.dp))
         Row {
             Box {
                 Button(onClick = {}) {
@@ -75,7 +78,7 @@ fun UnitConverter(modifier: Modifier = Modifier) {
             }
 
         }
-
+        Spacer(modifier = Modifier.padding(16.dp))
         Text("Result: ")
     }
 }
